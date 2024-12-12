9769d52c45dec6ab772a0f1c326d25ba0d2ec9a7
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:22:50 -0800

    Update cart.js

--- models/cart.js
+++ models/cart.js
@@ -7,27 +7,30 @@ const p = path.join(
   "cart.json"
 );
 
-module.exports = class Cart {
-  static addproduct(id, productPrice) {
-    // Fetch the previous cart
+module.exports = class Cart 
+{
+  static addproduct(id, productPrice) 
+  {
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
-      if (!err) {
+      if (!err) 
+      {
         cart = JSON.parse(fileContent);
       }
-      // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex(
         (prod) => prod.id === id
       );
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-      // Add new product / increase quantity
-      if (existingProduct) {
+      if (existingProduct) 
+      {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
         cart.products[existingProductIndex] = updatedProduct;
-      } else {
+      } 
+      else 
+      {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
@@ -38,14 +41,18 @@ module.exports = class Cart {
     });
   }
 
-  static deleteProduct(id, productPrice) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
+  static deleteProduct(id, productPrice) 
+  {
+    fs.readFile(p, (err, fileContent) => 
+      {
+      if (err) 
+      {
         return;
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((x) => x.id === id);
-      if (!product) {
+      if (!product) 
+      {
         return;
       }
       const productQty = product.qty;
