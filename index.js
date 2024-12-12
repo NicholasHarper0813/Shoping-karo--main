f447b51ce7caa78af828e0b0267f5b08b93abe75
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:21:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const PageNotFound = require("./helper/NotFound");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const PageNotFound = require("./helper/NotFound");
 const bodyParser = require("body-parser");
-const path = require("path");
-// const mongoConnect = require("./helper/database").mongoConnect;
 const User = require("./models/user");
 const mongoose = require("mongoose");
+const path = require("path");
 const mongo_DB_URI = process.env.MONGO_DB_URI;
-
 const app = express();
 const PORT = 3000;
-// app.set("view engine", "pug");
+
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-// const rootDir = require("./helper/path");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -32,9 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/admin", adminRoutes);
-
 app.use(shopRoutes);
-
 app.use(PageNotFound);
 
 mongoose
@@ -62,9 +56,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-// mongoConnect(() => {
-//   app.listen(PORT, () => {
-//     console.log("App is running on the port http://localhost:3000");
-//   });
-// });
